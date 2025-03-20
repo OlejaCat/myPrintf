e5639c667d3291c373b4ac527bb794e23f062312
OlejaCat <olegkatyskin@gmail.com>
2025-03-20 16:29:50 +0300

    Added conditional compilation

--- include/myPrintf.h
+++ include/myPrintf.h
@@ -1,6 +1,12 @@
 #ifndef MY_PRINTF_H
 #define MY_PRINTF_H
 
-int myPrintf(const char* format_string, ...);
+#ifdef __cplusplus
+    extern "C" int myPrintf(const char* format_string, ...)
+            __attribute__((format(printf, 1, 2)));
+#else
+    int myPrintf(const char* format_string, ...)
+            __attribute__((format(printf, 1, 2)));
+#endif
 
 #endif
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 nasm -f elf64 source/myPrintf.s -o myPrintf.o
-gcc -Iinclude -no-pie source/main.c myPrintf.o -o exec
+gcc -Iinclude -no-pie source/main.cpp myPrintf.o -o exec
