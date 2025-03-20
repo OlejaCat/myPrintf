922e13176bcf09076f3987e6bf04efbbfc2811c3
OlejaCat <olegkatyskin@gmail.com>
2025-03-20 16:36:51 +0300

    Another way of conditional compilation

--- include/myPrintf.h
+++ include/myPrintf.h
@@ -1,12 +1,15 @@
 #ifndef MY_PRINTF_H
 #define MY_PRINTF_H
 
-#ifdef __cplusplus
-    extern "C" int myPrintf(const char* format_string, ...)
-            __attribute__((format(printf, 1, 2)));
-#else
-    int myPrintf(const char* format_string, ...)
-            __attribute__((format(printf, 1, 2)));
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+int myPrintf(const char* format_string, ...)
+             __attribute__((format(printf, 1, 2)));
+
+#if defined(__cplusplus)
+}
 #endif
 
 #endif
